8ea226cf6c41c66fc88c2528f6a7ceaab41e79a9
kenterberiiskii <a.mizyakov@gmail.com>
2022-12-26 20:20:03 +0300

    test

---
+++ userlist/someusers/models.py
@@ -0,0 +1,10 @@
+from uuid import uuid4
+
+from django.db import models
+
+class SomeUser(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    username = models.CharField(max_length=64)
+    email = models.EmailField(max_length=120, unique=True)
